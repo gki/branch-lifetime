7e703f8dc0ab6e2295b5c65f2bb5c70f332928a1
Gen Takeda <hogehoge.gki@gmail.com>
2022-09-23 19:29:43 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const num : number = +process.argv[2];
+console.log(fizzbuzz(num));
+
+function fizzbuzz(num : number) : string {
+  if (num % 15 == 0) {
+    return "FizzBuzz";
+  } else if (num % 3 == 0) {
+    return "Fizz";
+  } else if (num % 5 == 0) {
+    return "Buzz";
+  }
+  return num.toString();
+}
